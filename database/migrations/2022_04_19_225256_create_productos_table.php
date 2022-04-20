7f95d8fd666d2a0c70795c36f843d0b08c30e08d
AlexOspinaBrain <eiji2l@gmail.com>
2022-04-19 20:28:23 -0500

    migrations

---
+++ database/migrations/2022_04_19_225256_create_productos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->increments('id_producto');
+            $table->string('tipo');
+            $table->integer('cantidad');
+            $table->integer('valor_unitario');
+            $table->integer('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
---
+++ database/migrations/2022_04_19_231814_create_pedidos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePedidosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pedidos', function (Blueprint $table) {
+            $table->increments('id_pedido');
+            
+            $table->integer('total_productos');
+            $table->integer('total_pedido');
+            $table->string('estado');
+            $table->timestamp('fecha_pago', $precision = 0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pedidos');
+    }
+}
